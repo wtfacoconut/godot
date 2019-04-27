efa00db68742df3a211d701bcea695f5a399b258
Ezekiel Bethel <zek@9net.org>
2019-04-27 23:05:51 +0100

    oops

--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
-export PATH=$PATH:/opt/devkitpro/devkitA64/bin:/opt/devkitpro/portlibs/switch/bin:/opt/devkitpro/toosl/bin
+export PATH=$PATH:/opt/devkitpro/devkitA64/bin:/opt/devkitpro/portlibs/switch/bin:/opt/devkitpro/tools/bin
 scons platform=switch tools=no module_etc_enabled=no module_webm_enabled=no module_websocket_enabled=no module_upnp_enabled=no $@ -j8
