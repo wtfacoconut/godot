3f7602fb1282fd3e13ec241a7bfade073aaf8635
cpasjuste <cpasjuste@gmail.com>
2019-05-13 10:41:01 +0200

    switch: enable etc2 texture compression feature in export template switch: "go.sh" to build debug target, add "go_release.sh" to build release targets

--- go.sh
+++ go.sh
@@ -1,5 +1,2 @@
 export PATH=$PATH:/opt/devkitpro/devkitA64/bin:/opt/devkitpro/portlibs/switch/bin:/opt/devkitpro/tools/bin
-scons platform=switch tools=no target=release module_etc_enabled=no module_webm_enabled=no module_websocket_enabled=no module_upnp_enabled=no $@ -j8
-# copy binary to godot templates for switch export
-mkdir -p ~/.local/share/godot/templates/3.1.1.stable/
-cp bin/switch_release.nro ~/.local/share/godot/templates/3.1.1.stable/
+scons platform=switch tools=no target=debug module_etc_enabled=no module_webm_enabled=no module_websocket_enabled=no module_upnp_enabled=no $@ -j8
