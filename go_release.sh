1b30f338883b5e649a3458914217a9f8baa3fa16
Ezekiel Bethel <zek@9net.org>
2019-06-07 00:46:15 +0100

    Update to enable webm module.

--- go_release.sh
+++ go_release.sh
@@ -8,13 +8,13 @@ mkdir -p ~/.local/share/godot/templates/3.1.1.stable/
 ###
 ## build switch release binary
 ###
-scons platform=switch tools=no target=release module_etc_enabled=no module_webm_enabled=no module_websocket_enabled=no module_upnp_enabled=no $@ -j8
+scons platform=switch tools=no target=release module_websocket_enabled=false module_upnp_enabled=no $@ -j8
 # copy binary to godot templates for switch export
 cp bin/switch_release.nro ~/.local/share/godot/templates/3.1.1.stable/
 
 ###
 ## build x11 editor binary (x64)
 ###
-scons platform=x11 tools=yes target=release_debug bits=64 module_etc_enabled=no module_webm_enabled=no module_websocket_enabled=no module_upnp_enabled=no $@ -j8
+scons platform=x11 tools=yes target=release_debug bits=64 module_websocket_enabled=no module_upnp_enabled=no $@ -j8
 # copy binary to godot templates for x11 export
 cp bin/godot.x11.opt.tools.64 ~/.local/share/godot/templates/3.1.1.stable/linux_x11_64_release
