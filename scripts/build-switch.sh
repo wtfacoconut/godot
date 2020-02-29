e5bc680f01d31d9477a4be58faba262d05dd6229
Ezekiel Bethel <zek@9net.org>
2020-02-29 17:14:45 +0000

    asdashngyfb

--- scripts/build-switch.sh
+++ scripts/build-switch.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 sudo apt-get install scons
-
 sudo dkp-pacman -S switch-pkg-config switch-freetype switch-bulletphysics switch-libtheora switch-libpcre2 switch-mesa switch-opusfile switch-mbedtls switch-libwebp switch-libvpx switch-miniupnpc
 scons platform=switch tools=no target=release_debug -j2
