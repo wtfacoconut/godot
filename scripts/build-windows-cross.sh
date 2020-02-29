f8e107c043cd689a70c4cd256dbd2898d762dc8f
Ezekiel Bethel <zek@9net.org>
2020-02-29 17:30:41 +0000

    aasd

--- scripts/build-windows-cross.sh
+++ scripts/build-windows-cross.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get install mingw-w64 scons
-sudo update-alternatives --set x86_64-w64-mingw32-gcc x86_64-w64-mingw32-gcc-posix
-sudo update-alternatives --set x86_64-w64-mingw32-g++ x86_64-w64-mingw32-g++-posix
+sudo update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix
+sudo update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
 
 scons platform=windows tools=yes target=release_debug -j2
