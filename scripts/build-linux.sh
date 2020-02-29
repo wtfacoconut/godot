20cbbe407a84510953d65be243e26d6738af6317
Ezekiel Bethel <zek@9net.org>
2020-02-29 16:01:12 +0000

    oops install deps

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,5 +1,9 @@
-cd godot
+#!/usr/bin/env bash
+
+sudo apt-get install build-essential scons pkg-config libx11-dev libxcursor-dev libxinerama-dev \
+    libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libfreetype6-dev libudev-dev libxi-dev \
+    libxrandr-dev yasm
+
 scons platform=x11 tools=yes target=release_debug \
-      udev=yes use_static_cpp=yes \
-      "${SCONS_FLAGS[@]}"
+      udev=yes use_static_cpp=yes
 # TODO: AppImage?
