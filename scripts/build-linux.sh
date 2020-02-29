71194cb940cd3e4cf22ea968524604f362d00263
Ezekiel Bethel <zek@9net.org>
2020-02-29 16:19:09 +0000

    ok

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -7,7 +7,7 @@ sudo apt-get install build-essential scons pkg-config libx11-dev libxcursor-dev 
 # There's a compiler error. Let's uhhhhhh silence it.
 # TODO: Fix this.
 scons platform=x11 tools=yes target=release_debug \
-      udev=yes use_static_cpp=yes progress=no CCFLAGS=-Wno-strict-aliasing -j2
+      udev=yes use_static_cpp=yes progress=no CXXFLAGS=-fno-strict-aliasing -j2
 
 # Create Linux editor AppImage
 strip "bin/godot.x11.opt.tools.64"
